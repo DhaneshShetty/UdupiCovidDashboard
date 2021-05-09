c43f56c5bd6ed66aa9a7c8a49baeb8386ab6b4c3
Dhanesh Shetty <dhaneshshetty65@gmail.com>
2021-05-09 12:39:05 +0530

    Added counter and minor changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {Cases} from './Cases';
 import {Vaccine} from './Vaccine';
 import {Slots} from './Slots';
-import {OtherInfo} from './OtherInfo';
+import {OtherInfo, YourComponent} from './OtherInfo';
 import React from 'react';
 
 function App() {
@@ -17,11 +17,11 @@ function App() {
       <OtherInfo/> 
       <footer style={{textAlign:"center",color:"white",backgroundColor:"#f64C72",padding:'10px',marginTop:'10px'}} className="container-fluid">
             A voluntary initiative for the people of Udupi<br/>
-            Illustrations: undraw.co, Icon: fontawesome.com
-      </footer>
+            Illustrations: undraw.co, Icon: fontawesome.com <br/>
+            <YourComponent/>           
+      </footer>            
     </div>
   );
 }
 
 export default App;
-//https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=16&district_id=286&date=2021-05-07
--- src/Cases.js
+++ src/Cases.js
@@ -23,7 +23,7 @@ export function Cases(){
             <h3 style={{color:'#f64C72',margin:'20px'}}>COVID Stats</h3>
             <div className="row">
                 <div className="col-sm-3 boxes total"><h5>Total</h5><h6>{totalCases.cases}</h6>+{totalCases.delta}</div>
-                <div className="col-sm-3 boxes active"><h5>Active</h5><h6>{activeCases.cases}</h6>{activeCases.delta}</div>
+                <div className="col-sm-3 boxes active"><h5>Active</h5><h6>{activeCases.cases}</h6>{Number(activeCases.delta)>=0 ? "+"+activeCases.delta : activeCases.delta }</div>
                 <div className="col-sm-3 boxes recover"><h5>Recovered</h5><h6>{recoveredCases.cases}</h6>+{recoveredCases.delta}</div>
                 <div className="col-sm-3 boxes dead"><h5>Deceased</h5><h6>{deceasedCases.cases}</h6>+{deceasedCases.delta}</div>
             </div>
