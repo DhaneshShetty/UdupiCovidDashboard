7e3104f0fb25014817cb2128705f20a6eaf5e6e6
Dhanesh Shetty <dhaneshshetty65@gmail.com>
2021-06-26 14:36:46 +0530

    Added filters to slots

--- src/Slots.js
+++ src/Slots.js
@@ -4,6 +4,13 @@ export function Slots(){
     var today = new Date();
     const [slots,setSlots] = useState([]);
     const [date,setDate] = useState(today);
+    const [age18,setAge18] = useState(false);
+    const [free,setFree] = useState(false);
+    const [paid,setPaid] = useState(false);
+    const [covaxin,setCovaxin] = useState(false);
+    const [covishield,setCovishield] = useState(false);
+    const [dose1,setDose1] = useState(false);
+    const [dose2,setDose2] = useState(false);
     useEffect(()=>{    
         var dateFetch = new Date(date);
         var dd = String(dateFetch.getDate()).padStart(2, '0');
@@ -25,20 +32,92 @@ export function Slots(){
         console.log(newDate.getDate);
         setDate(newDate);
     }
+
+    const toggleBackground=(e,state)=>{
+        if(state){
+            e.target.style.backgroundColor="#242582";
+        }
+        else{
+            e.target.style.backgroundColor="#f64C72";
+        } 
+    }
+
+    const filter18=(e)=>{
+        toggleBackground(e,age18);              
+        setAge18(!age18);
+    }
+
+    const filterFree=(e)=>{
+        toggleBackground(e,free);      
+        setFree(!free);
+    }
+
+    const filterPaid=(e)=>{
+        toggleBackground(e,paid)     
+        setPaid(!paid);
+    }
+
+    const filterDose1=(e)=>{
+        toggleBackground(e,dose1);     
+        setDose1(!dose1);
+    }
+
+    const filterDose2=(e)=>{
+        toggleBackground(e,dose2);      
+        setDose2(!dose2);
+    }
+
+    const filterCovaxin=(e)=>{
+        toggleBackground(e,covaxin);      
+        setCovaxin(!covaxin);
+    }
+
+    const filterCovisheild=(e)=>{
+        toggleBackground(e,covishield);     
+        setCovishield(!covishield);
+    }
     return (
     <div>
         <h3 style={{color:'#f64C72',margin:'20px'}}>Vaccine Slots Availablility</h3>
-        <input type="date" onChange={handleChange} style={{margin:"20px",color:"#f64C72",backgroundColor:"#242582"}} value={date.getFullYear()+"-"+String(date.getMonth() + 1).padStart(2, '0')+"-"+String(date.getDate()).padStart(2, '0')}/><br/>
+        <input type="date" onChange={handleChange} style={{margin:"20px",color:"#f64C72",backgroundColor:"#242582"}} value={date.getFullYear()+"-"+String(date.getMonth() + 1).padStart(2, '0')+"-"+String(date.getDate()).padStart(2, '0')}/>
+        <span style={{backgroundColor:"#242582",color:"#FFFFFF",borderRadius:"5px",width:"50px",margin:"8px",padding:"8px"}} onClick={filter18}>18+</span>
+        <span style={{backgroundColor:"#242582",color:"#FFFFFF",borderRadius:"5px",width:"50px",margin:"8px",padding:"8px"}} onClick={filterFree}>Free</span>
+        <span style={{backgroundColor:"#242582",color:"#FFFFFF",borderRadius:"5px",width:"50px",margin:"8px",padding:"8px"}} onClick={filterPaid}>Paid</span><wbr/>
+        <span style={{backgroundColor:"#242582",color:"#FFFFFF",borderRadius:"5px",width:"50px",margin:"8px",padding:"8px"}} onClick={filterCovaxin}>Covaxin</span>
+        <span style={{backgroundColor:"#242582",color:"#FFFFFF",borderRadius:"5px",width:"50px",margin:"8px",padding:"8px"}} onClick={filterCovisheild}>Covishield</span><wbr/>
+        <span style={{backgroundColor:"#242582",color:"#FFFFFF",borderRadius:"5px",width:"50px",margin:"8px",padding:"8px"}} onClick={filterDose1}>Dose 1</span>
+        <span style={{backgroundColor:"#242582",color:"#FFFFFF",borderRadius:"5px",width:"50px",margin:"8px",padding:"8px"}} onClick={filterDose2}>Dose 2</span><br/>
         <div>
             {slots.length===0 ? <div className="boxes"> No Slots Available for this Date </div>:
-            slots.map(center => (
+            slots.filter(it=>{if(age18)
+                {return it.min_age_limit===18}
+                else{return true}
+            }).filter(it=>{if(free)
+                {return it.fee_type==="Free"}
+                else{return true}
+            }).filter(it=>{if(paid)
+                {return it.fee_type==="Paid"}
+                else{return true}
+            }).filter(it=>{if(covaxin)
+                {return it.vaccine==="COVAXIN"}
+                else{return true}
+            }).filter(it=>{if(covishield)
+                {return it.vaccine==="COVISHIELD"}
+                else{return true}
+            }).filter(it=>{if(dose1)
+                {return it.available_capacity_dose1>0}
+                else{return true}
+            }).filter(it=>{if(dose2)
+                {return it.available_capacity_dose2>0}
+                else{return true}
+            }).map(center => (
             <div className="row boxes">
                 <div className="col-6"> 
                     <p>{center.name}<br/>
                     {center.address}</p>
                 </div>
                 <div className="col-6">
-                <p>Slots:{center.available_capacity}    Vaccine:{center.vaccine}<br/>Fee:{center.fee_type}      Age:{center.min_age_limit}+<br/>
+                <p>Dose 1:{center.available_capacity_dose1}  Dose 2:{center.available_capacity_dose2} <br/> Vaccine:{center.vaccine}<br/>Fee:{center.fee}      Age:{center.min_age_limit}+<br/>
                 Timings:{center.from}-{center.to}</p>
                 </div>
             </div>
@@ -48,10 +127,3 @@ export function Slots(){
     </div>);
 }
 
-//<div class="mapouter">
-//           <div class="gmap_canvas">
-//              <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Udupi%20District,Karnataka&t=&z=9&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-//              <a href="https://123movies-to.org"></a><br/>
-//             <a href="https://www.embedgooglemap.net">embed google maps in website</a>
-//          </div>
-//      </div>
